c8f3dd148060d67dad1c55f5878ed5a06355b513
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-13 14:07:14 +0100

    feat: add a user authentification system and configure email system

--- src/Controller/Admin/ProjectController.php
+++ src/Controller/Admin/ProjectController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/project', name: 'admin.project')]
+#[IsGranted('ROLE_ADMIN')]
 class ProjectController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: '.index')]
     public function index(ProjectRepository $repository): Response
     {
         $projects = $repository->findAll();
