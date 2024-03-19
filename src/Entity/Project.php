95bc2a1a6b6fdb73c295c320bbf75c0ad1412102
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-19 14:38:51 +0100

    install php stan symfony extension

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[ApiResource]
 class Project
 {
     #[ORM\Id]
