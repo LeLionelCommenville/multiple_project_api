6c960109ae582dc6c8c8878c5b2fcfb954de84d3
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-19 14:43:32 +0100

    fix: unreachable statement on appauthenticator

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,10 +49,6 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         return new RedirectResponse($this->urlGenerator->generate('admin.project.index'));
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
